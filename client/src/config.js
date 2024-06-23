fa95bd67fa554d80a939962b7a29840462ebfc14
Alexander0131 <acidinit@gmail.com>
2024-06-23 08:40:02 +0000

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,5 @@
 
-// export const apiUrl =  "http://192.168.104.91:5000"
-// export const apiUrl =  "http://172.16.6.55:5000" ? "http://172.16.6.55:5000" : "http://localhost:5000";
-export const apiUrl =  "http://localhost:5000"
-// http://172.16.6.55/
+export const apiUrl =  "https://defex-api-0ijs.onrender.com"
    
 export const imgLoad = apiUrl + "/"
 
