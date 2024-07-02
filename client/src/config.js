4344c629d01db3725d825ce1c5ec51db636263ec
Alexander0131 <acidinit@gmail.com>
2024-07-02 00:50:09 -0700

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-  export const apiUrl =  "https://defex-api.onrender.com"
+ export const apiUrl =  "https://defex-api.onrender.com"
 //export const apiUrl =  "http://localhost:5000"
 
 //export const apiUrl = "https://defex-api.vercel.app"
